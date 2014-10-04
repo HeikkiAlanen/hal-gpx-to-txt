bba36a628a2ff0654e9a7c91a4cb3a5c4488b709
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-10-04 23:14:51 +0300

    Name should not contain comma, fullstop etc. Part1

--- gpx-to-txt.js
+++ gpx-to-txt.js
@@ -2,14 +2,17 @@ var fs = require('fs')
 var xml2js = require('xml2js');
 
 var parser = new xml2js.Parser();
-fs.readFile('./passit/passit_20131118-ks.gpx', function(err, data) {
+fs.readFile('./passit/passit_20131118-wo-track.gpx', function(err, data) {
     parser.parseString(data, function (err, result) {
 		var poiCount = result.gpx.wpt.length;
 		var pois = result.gpx.wpt;
 		var poiString = "";
 
 		for (i = 0; i < poiCount; i++) {
-			poiString += pois[i].name + "," + pois[i].$.lat + "," + pois[i].$.lon + "\r\n";
+			var poiName = String(pois[i].name).replace(",", ";", "g");
+			//var newPoiName = poiName.replace(",", ";", "g");
+			//var newPoiName2 = newPoiName.replace(',', ';', 'g');
+			poiString +=  poiName + "," + pois[i].$.lat + "," + pois[i].$.lon + "\r\n";
 		}
 		
 		fs.writeFile('./passit/passit.txt', poiString, function (err) {
